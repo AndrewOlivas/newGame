5df3e1c524f50ac941b0a93a402697e0ae5db8dd
Andrew Olivas <andrewrolivas@gmail.com>
2015-01-23 08:51:44 -0800

    video 7, complete with no errors but player wont move(confused)

--- js/game.js
+++ js/game.js
@@ -41,7 +41,6 @@ var game = {
 	"loaded" : function () {
 		me.pool.register("player", game.PlayerEntity, true);
 
-
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
 
